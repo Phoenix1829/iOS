a317b8096100ea0b47ee00720b35a57af2e1dc97
Mikhail <mishakonshu03@gmail.com>
2023-11-18 07:30:11 -0800

    init commit

---
+++ iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by misha on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
